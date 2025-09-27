2cc240d173fd570a600a7391a1832b5cb4e8cc49
Kyle Prestado <kylerossprestado@gmail.com>
2025-09-27 11:39:59 +0800

    add animation for headers

--- components/ui/typography.tsx
+++ components/ui/typography.tsx
@@ -1,6 +1,12 @@
+"use client";
 import { type ComponentProps } from "react";
 import styles from "@/assets/css/ui/typography.module.scss";
 import clsx from "clsx";
+import React, { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { SplitText } from "gsap/SplitText";
 
 const Caption = ({ children, className, ...props }: ComponentProps<"p">) => {
   return (
@@ -11,8 +17,38 @@ const Caption = ({ children, className, ...props }: ComponentProps<"p">) => {
 };
 
 const Header = ({ children, className, ...props }: ComponentProps<"h2">) => {
+  const headerRef = useRef(null);
+
+  useGSAP(() => {
+    gsap.registerPlugin(ScrollTrigger, SplitText);
+    if (headerRef.current) {
+      headerRef.current.style.opacity = 1;
+      const splitText = new SplitText(headerRef.current, {
+        type: "words, chars",
+      });
+
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: headerRef.current,
+          toggleActions: "restart pause resume reverse",
+          start: "top 90%",
+        },
+      });
+
+      tl.from(splitText.chars, {
+        duration: 0.4,
+        opacity: 0,
+        y: 120,
+        ease: "power1.out",
+        stagger: 0.01,
+        onComplete: () => {
+          headerRef.current.classList.add(`animated`);
+        },
+      });
+    }
+  });
   return (
-    <h2 className={clsx(styles.header)} {...props}>
+    <h2 className={clsx(styles.header)} ref={headerRef} {...props}>
       {children}
     </h2>
   );
